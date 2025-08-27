e28c77b5f643bf8d28b183e8768943d4e69ff1ac
zeyadthereal <zeyadthereal@gmail.com>
2025-08-27 15:18:21 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,22 @@ import JsonLd from "../components/JsonLd";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import TrustBar from "../components/TrustBar";
+
 import ValueProps from "../components/ValueProps";
+
 import HowItWorks from "../components/HowItWorks";
+
 import CaseStudy from "../components/CaseStudy";
+
 import Pricing from "../components/Pricing";
+
 import FAQ from "../components/FAQ";
+
 import CTA from "../components/CTA";
+
 import Footer from "../components/Footer";
+
+import type { Lang } from "../lib/i18n";
 
 
 export default function Home() {
